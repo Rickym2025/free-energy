b4fa21f734c219d0ed32c092591a56dba5bf1156
Riccardo Modena <modena.riccardo@gmail.com>
2026-07-12 09:47:14 +0200

    Update page.tsx

--- src/app/dashboard/pv-planner/page.tsx
+++ src/app/dashboard/pv-planner/page.tsx
@@ -2,7 +2,7 @@
 "use client";
 
 import React, { useEffect, useState, useRef } from 'react';
-import { useTenant } from '@/context/TenantContext';
+import { useTenant } from '../../../context/TenantContext';
 
 interface Coordinate {
   lat: number;
