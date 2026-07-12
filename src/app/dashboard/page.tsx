66ab761c2e790d15da1a2a20d9909fb2f643316b
Riccardo Modena <modena.riccardo@gmail.com>
2026-07-12 09:53:48 +0200

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import Link from 'next/link';
-import { useTenant } from '@/context/TenantContext';
+import { useTenant } from '@/app/context/TenantContext';
 
 export default function DashboardHome() {
   const { tenant } = useTenant();
