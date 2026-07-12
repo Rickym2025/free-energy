a4129869226780ebdad402f2e4b84808e503c963
Riccardo Modena <modena.riccardo@gmail.com>
2026-07-12 09:51:57 +0200

    Update page.tsx

--- src/app/dashboard/cv-evaluator/page.tsx
+++ src/app/dashboard/cv-evaluator/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from 'react';
-import { useTenant } from '../../../context/TenantContext';
+import { useTenant } from '@/context/TenantContext';
 
 interface Candidate {
   id: string;
